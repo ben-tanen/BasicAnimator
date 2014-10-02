aa965b8fc72a54d03fe3ad1e5a78e9d61685d1ec
Ben Tanen <ben.tanen@gmail.com>
2014-10-02 15:32:47 -0400

    started animObject class

--- multi-sprite/animObject.cpp
+++ multi-sprite/animObject.cpp
@@ -6,12 +6,31 @@
 
 using namespace std;
 
-animObject::animObject() {
+// constructor (with set img_num and img_size)
+animObject::animObject(int img_num, int img_hgt, int img_wid) {
+	img_num = img_num;
+	img_hgt = img_hgt;
+	img_wid = img_wid;
+
+	read();
+}
+
+// read in the parameters and sprites of the animObject
+// return true if successful (false if not)
+bool animObject::read() {
+	// insert code here
+
+	return true;
+}
+
+// draw the object (current image sprite)
+void animObject::draw() {
 
 }
 
-animObject::~animObject() {
-	
+// loop through all object sprites and draw them (at certain fps)
+void animObject::animate(int fps) {
+
 }
 
 
--- multi-sprite/animObject.h
+++ multi-sprite/animObject.h
@@ -6,11 +6,24 @@ using std::string;
 
 class animObject {
 	public:
-		animObject();
-		~animObject();
+		animObject(int img_num, int img_hgt, int img_wid);
+
+		bool read();
+		void draw();
+		void animate(int fps);
+		
 	
 	private:
-		
+		// max number of sprites per object
+		static const int max_img = 15;
+		static const int max_hgt = 50;
+		static const int max_wid = 50;
+
+		int img_num;
+		int img_hgt;
+		int img_wid;
+
+		char images[max_img][max_hgt][max_wid];
 };
 
 #endif
